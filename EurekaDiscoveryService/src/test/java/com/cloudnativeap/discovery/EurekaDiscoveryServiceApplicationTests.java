5eb0250b4c3983df246d0e56a27f78125426a787
gitrizcky <67969694+gitrizcky@users.noreply.github.com>
2020-07-07 23:15:54 +0700

    Add files via upload

---
+++ EurekaDiscoveryService/src/test/java/com/cloudnativeap/discovery/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudnativeap.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
